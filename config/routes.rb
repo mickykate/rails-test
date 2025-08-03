e04afaefbf11a3daece9d48f15da2b02042cda3e
mickykate <ka12miyuki@gmail.com>
2025-08-03 16:10:29 +0900

    ログイン・ログアウト機能を作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'posts/index' , to: 'posts#index'
   #get 'posts/new' , to: 'posts#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
+  root 'posts#index'
 end
