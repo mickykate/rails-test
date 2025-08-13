4602d64033f1398ad07b3074ce47e45b313a9801
mickykate <ka12miyuki@gmail.com>
2025-08-13 23:17:10 +0900

    トップページを作成。そこからログイン・新規当ロキ画面に進める流れを作成した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'posts/index' , to: 'posts#index'
-  #get 'posts/new' , to: 'posts#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  root 'posts#index'
+  
+  # ルートページをホームに設定
+  root 'home#index'
 end
